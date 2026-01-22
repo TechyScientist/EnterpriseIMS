07c45dd08d47105cfcf23ea9d417b2c01d1965d5
Johnny Console <johnny.console@hotmail.com>
2026-01-22 13:57:14 -0500

    Add sign in API functionality

--- Base/ims-api/src/main/java/com/johnnyconsole/enterpriseims/api/classes/AuthApi.java
+++ Base/ims-api/src/main/java/com/johnnyconsole/enterpriseims/api/classes/AuthApi.java
@@ -1,17 +1,22 @@
 package com.johnnyconsole.enterpriseims.api.classes;
 
+import com.johnnyconsole.enterpriseims.persistence.User;
 import com.johnnyconsole.enterpriseims.persistence.interfaces.UserDao;
 
 import javax.ejb.EJB;
-import javax.ws.rs.FormParam;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("/auth")
+@Stateless
 public class AuthApi {
 
     @EJB
@@ -19,9 +24,45 @@ public class AuthApi {
 
     @POST
     @Path("/sign-in")
+    @Consumes(APPLICATION_FORM_URLENCODED)
     @Produces(APPLICATION_JSON)
-    public Response signIn(@FormParam("username") String username, @FormParam("password") String password) {
-        //TODO: Check entered information and build the response object
-        return Response.ok().build();
+    public Response signIn(@FormParam("username") String username,
+                           @FormParam("password") String password) {
+        int status = 200;
+        String json;
+        if (username != null && password != null &&
+                !username.isEmpty() && !password.isEmpty()) {
+            if (userDao.userExists(username)) {
+                User user = userDao.getUser(username);
+
+                if (userDao.verifyUserPassword(user, password)) {
+                    json = "{\n\t" +
+                            "\"username\": \"" + user.getUsername() + "\",\n\t" +
+                            "\"name\": \"" + user.getName() + "\",\n\t" +
+                            "\"is_administrator\": " + user.isAdministrator() + "\n" +
+                            "}";
+                } else {
+                    status = 401;
+                    json = "{\n\t" +
+                            "\"error\": \"401 (Unauthorized)\",\n\t" +
+                            "\"message\": \"Invalid credentials, please try again\"\n" +
+                            "}";
+                }
+            } else {
+                status = 401;
+                json = "{\n\t" +
+                        "\"error\": \"401 (Unauthorized)\",\n\t" +
+                        "\"message\": \"Invalid credentials, please try again.\"\n" +
+                        "}";
+            }
+        } else {
+            status = 400;
+            json = "{\n\t" +
+                    "\"error\": \"400 (Bad Request)\",\n\t" +
+                    "\"message\": \"Missing username or password.\"\n" +
+                    "}";
+        }
+
+        return Response.status(status).entity(json).build();
     }
 }
