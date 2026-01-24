8d4542790a6610600ee5a44b54a91ee2930cbb7f
Johnny Console <johnny.console@hotmail.com>
2026-01-23 20:46:33 -0500

    Add empty dashboard activity

---
+++ Android/app/src/main/java/com/johnnyconsole/enterpriseims/android/DashboardActivity.kt
@@ -0,0 +1,27 @@
+package com.johnnyconsole.enterpriseims.android
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.johnnyconsole.enterpriseims.android.databinding.ActivityDashboardBinding
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        with(binding) {
+            setContentView(root)
+            ViewCompat.setOnApplyWindowInsetsListener(main) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+    }
+}
