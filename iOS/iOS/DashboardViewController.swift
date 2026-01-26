2fb37cc70ef4a9ad5d12099ea500f72686e8da50
Johnny Console <johnny.console@hotmail.com>
2026-01-26 16:17:29 -0500

    Display signed in username, prompt user to confirm signing out

--- iOS/iOS/DashboardViewController.swift
+++ iOS/iOS/DashboardViewController.swift
@@ -11,14 +11,33 @@ class DashboardViewController: UIViewController {
     
     private var user: User?
     
+    @IBOutlet weak var lbUsername: UILabel!
+    @IBOutlet weak var adminBadge: UIView!
+    @IBOutlet weak var btAdministration: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
+        
+        lbUsername.text = user!.username
+        if(user!.administrator) {
+            adminBadge.isHidden = false
+            btAdministration.isHidden = false
+        }
+        
     }
     
     func with(_ initUser: User) {
         user = initUser
+    }
+    
+    @IBAction func onSignOut() {
+        let controller = UIAlertController(title: "Confirm Sign Out", message: "Are you sure you want to sign out?", preferredStyle: .alert)
+        controller.addAction(UIAlertAction(title: "No", style: .cancel))
+        controller.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { _ in
+            self.performSegue(withIdentifier: "SignOut", sender: nil)
+        }))
+        present(controller, animated: true)
     }
 }
 
