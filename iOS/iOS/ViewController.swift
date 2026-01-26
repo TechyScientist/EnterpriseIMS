d2c38fdfff4ccdf696a0c9d0ece5b0fbd5c91163
Johnny Console <johnny.console@hotmail.com>
2026-01-26 13:46:44 -0500

    Make web request to sign in

--- iOS/iOS/ViewController.swift
+++ iOS/iOS/ViewController.swift
@@ -22,7 +22,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onSignIn() {
-        
+        errorView.isHidden = true
+        indicator.isHidden = false
+        var request = URLRequest(url: URL(string: "https://wildfly.johnnyconsole.com:8443/ims/api/auth/sign-in")!)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "username=\(tfUsername.text ?? "")&password=\(tfPassword.text ?? "")".data(using: .utf8)
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: request) { [weak self] data, response, error in
+            let responseCode = (response as! HTTPURLResponse).statusCode
+            
+            DispatchQueue.main.async {
+                if(responseCode == 200) {
+                    //TODO: Interpret user response, implement storyboard segue to dashboard activity, pass user information to next view controller
+                    print("Signed In!")
+                }
+                else {
+                    let errorText = switch(responseCode) {
+                        case 401: "Invalid credentials, please try again."
+                        default: "Missing credentials, please try again."
+                    }
+                    self!.lbError.text = "Error: \(errorText)"
+                    print(self!.lbError.text ?? "NOTHING")
+                    self!.errorView.sizeToFit()
+                    self!.errorView.isHidden = false
+                }
+                self!.indicator.isHidden = true
+            }
+        }.resume()
     }
 
 
