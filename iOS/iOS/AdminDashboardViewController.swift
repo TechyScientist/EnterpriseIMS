41b1a7490857e419ddd01a715bb6e28ef1dae1a7
Johnny Console <johnny.console@hotmail.com>
2026-01-27 13:19:35 -0500

    Add files for Administration view controller

---
+++ iOS/iOS/AdminDashboardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AdminDashboardViewController.swift
+//  iOS
+//
+//  Created by Johnny Console on 2026-01-27.
+//
+import UIKit
+
+class AdminDashboardViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
