366bbd6b743fdb6fcf6947949bee219de90c70bf
Johnny Console <johnny.console@hotmail.com>
2026-01-27 13:44:09 -0500

    Add functionality to the back button

--- iOS/iOS/AdminDashboardViewController.swift
+++ iOS/iOS/AdminDashboardViewController.swift
@@ -8,9 +8,19 @@ import UIKit
 
 class AdminDashboardViewController: UIViewController {
     
+    private var user: User?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func with(_ adminUser: User) {
+        user = adminUser
+    }
+    
+    @IBAction func onBack() {
+        dismiss(animated: true)
     }
 }
 
