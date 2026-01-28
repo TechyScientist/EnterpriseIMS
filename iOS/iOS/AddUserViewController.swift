7883e11157d4048e0d23b7782f6411551c6ba2c2
Johnny Console <johnny.console@hotmail.com>
2026-01-28 17:36:43 -0500

    Add files for Add User View Controller

---
+++ iOS/iOS/AddUserViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddUserViewController.swift
+//  iOS
+//
+//  Created by Johnny Console on 2026-01-28.
+//
+import UIKit
+
+class AddUserViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
