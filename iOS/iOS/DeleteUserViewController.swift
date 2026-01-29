f9309ebefe2e4c66457aa2023a9d73e76a1f9225
Johnny Console <johnny.console@hotmail.com>
2026-01-29 10:25:53 -0500

    Implement checking of response codes, disable user switch button if no users are returned

--- iOS/iOS/DeleteUserViewController.swift
+++ iOS/iOS/DeleteUserViewController.swift
@@ -30,29 +30,35 @@ class DeleteUserViewController: UIViewController {
         let request = URLRequest(url: URL(string: "https://wildfly.johnnyconsole.com:8443/ims/api/user/get-all?except=\(admin!.username)&auth-user=\(admin!.username)")!)
         let session = URLSession(configuration: .default)
         session.dataTask(with: request) { data, response, _ in
-            //TODO: Implement checking for the request response code
-            if let data = data {
-                
-                DispatchQueue.main.async { [self] in
+            let responseCode = (response as! HTTPURLResponse).statusCode
+            
+            DispatchQueue.main.async { [self] in
+                if responseCode == StatusCode.OK, let data = data {
                     self.users = try! JSONDecoder().decode([User].self, from: data)
                     
-                    var actions: [UIAction] = []
-                    
-                    for i in self.users.indices {
-                        actions.append(UIAction(title: "\(users[i].name) (\(users[i].username))", handler: {action in
-                            self.onChangeUser(i)
-                        }))
+                    if(!self.users.isEmpty) {
+                        var actions: [UIAction] = []
+                        
+                        for i in self.users.indices {
+                            actions.append(UIAction(title: "\(users[i].name) (\(users[i].username))", handler: {action in
+                                self.onChangeUser(i)
+                            }))
+                        }
+                        
+                        btUserSelect.menu = UIMenu(options: .singleSelection, children: actions)
+                        btUserSelect.setTitle(actions.first!.title, for: .normal)
+                        (btUserSelect.menu!.children.first as? UIAction)?.state = .on
+                        btUserSelect.changesSelectionAsPrimaryAction = false
+                        btUserSelect.showsMenuAsPrimaryAction = true
+                        btUserSelect.isEnabled = false
+                    } else {
+                        btUserSelect.setTitle("No Users Found", for: .normal)
+                        btUserSelect.isEnabled = false
                     }
-                    
-                    btUserSelect.menu = UIMenu(options: .singleSelection, children: actions)
-                    btUserSelect.setTitle(actions.first?.title ?? "No Users Found", for: .normal)
-                    //TODO: disable the submit button if there are no users to deleete
-                    (btUserSelect.menu!.children.first as? UIAction)?.state = .on
-                    btUserSelect.changesSelectionAsPrimaryAction = false
-                    btUserSelect.showsMenuAsPrimaryAction = true
                     indicator.isHidden = true
                     btUserSelect.isHidden = false
                 }
+                //TODO: Implement error checking from API response codes
             }
         }.resume()
     }
