ea6a2935c1a8564972977c0be7fc9152e7dd3440
Johnny Console <johnny.console@hotmail.com>
2026-01-30 11:46:39 -0500

    Get user list asynchronously

--- Android/app/src/main/java/com/johnnyconsole/enterpriseims/android/DeleteUserActivity.kt
+++ Android/app/src/main/java/com/johnnyconsole/enterpriseims/android/DeleteUserActivity.kt
@@ -1,23 +1,24 @@
 package com.johnnyconsole.enterpriseims.android
 
-import android.content.DialogInterface.BUTTON_NEGATIVE
-import android.content.DialogInterface.BUTTON_POSITIVE
 import android.os.AsyncTask
 import android.os.Bundle
 import android.text.Html
 import android.text.Html.FROM_HTML_MODE_LEGACY
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.ArrayAdapter
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.johnnyconsole.enterpriseims.android.databinding.ActivityAddUserBinding
+import com.johnnyconsole.enterpriseims.android.databinding.ActivityDeleteUserBinding
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection.HTTP_ACCEPTED
 import java.net.HttpURLConnection.HTTP_BAD_REQUEST
-import java.net.HttpURLConnection.HTTP_CONFLICT
-import java.net.HttpURLConnection.HTTP_CREATED
 import java.net.HttpURLConnection.HTTP_NOT_FOUND
 import java.net.HttpURLConnection.HTTP_UNAUTHORIZED
 import java.net.URL
@@ -26,11 +27,103 @@ import javax.net.ssl.HttpsURLConnection
 
 class DeleteUserActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityAddUserBinding
+    private lateinit var binding: ActivityDeleteUserBinding
+    private var users = ArrayList<String>()
+
+    private inner class UserAdapter: ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item) {
+
+        override fun getCount(): Int {
+            return users.size
+        }
+
+        override fun getItem(position: Int): String {
+            return users[position]
+        }
+
+    }
+
+    private inner class GetAllUsersTask: AsyncTask<String, Unit, Unit>() {
+
+        var response = HTTP_ACCEPTED
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            binding.tvError.visibility = GONE
+            binding.tvSuccess.visibility = GONE
+            binding.indicator.visibility = VISIBLE
+            binding.btSubmit.isEnabled = false
+
+        }
+
+        override fun doInBackground(vararg params: String?) {
+            val conn = URL("https://wildfly.johnnyconsole.com:8443/ims/api/user/get-all?except=${params[0]}&auth-user=${params[0]}")
+                .openConnection() as HttpsURLConnection
+            conn.doInput = true
+            conn.hostnameVerifier = HostnameVerifier { _, _ -> true }
+            conn.connect()
+            response = conn.responseCode
+
+            if(response == HTTP_ACCEPTED) {
+                val json = StringBuffer()
+                with(BufferedReader(InputStreamReader(conn.inputStream))) {
+                    for (line in readLines()) {
+                        json.append(line)
+                    }
+                }
+
+                val array = JSONArray(json.toString())
+                for (i in 0 until array.length()) {
+                    val user = array.get(i) as JSONObject
+                    users.add("${user.getString("name")} (${user.getString("username")})")
+                }
+            }
+            conn.disconnect()
+        }
+
+        override fun onPostExecute(result: Unit) {
+            super.onPostExecute(result)
+            with(binding) {
+                indicator.visibility = GONE
+                if(response == HTTP_ACCEPTED) {
+                    (spUser.adapter as UserAdapter).notifyDataSetChanged()
+                    btSubmit.isEnabled = true
+                }
+                else {
+                    tvError.visibility = VISIBLE
+                    tvError.text = Html.fromHtml(getString(R.string.error_message,
+                        when(response) {
+                            HTTP_BAD_REQUEST -> "Missing or empty parameter, please try again."
+                            HTTP_UNAUTHORIZED -> "User ${intent.getStringExtra("username")} is not an administrator."
+                            HTTP_NOT_FOUND -> "User ${intent.getStringExtra("username")} is not found."
+                            else -> "Unexpected HTTP response code: $response."
+                        }
+                    ), FROM_HTML_MODE_LEGACY)
+                }
+            }
+        }
+
+    }
+
+    private inner class DeleteUserTask: AsyncTask<String, Unit, Unit>() {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            binding.tvError.visibility = GONE
+            binding.tvSuccess.visibility = GONE
+            binding.indicator.visibility = VISIBLE
+        }
+
+        override fun doInBackground(vararg params: String?) {
+
+        }
+
+        override fun onPostExecute(result: Unit) {
+            super.onPostExecute(result)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAddUserBinding.inflate(layoutInflater)
+        binding = ActivityDeleteUserBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         with(binding) {
             setContentView(root)
@@ -41,6 +134,9 @@ class DeleteUserActivity : AppCompatActivity() {
             }
 
             btBack.setOnClickListener { _ -> finish() }
+            spUser.adapter = UserAdapter()
+            GetAllUsersTask()
+                .execute(this@DeleteUserActivity.intent.getStringExtra("username"))
         }
 
         onBackPressedDispatcher.addCallback(
